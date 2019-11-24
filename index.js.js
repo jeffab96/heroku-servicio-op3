5c94d0f97e4fecf24b8216ac384b075ab13eff7c
jeffab96 <jfmasache@uce.edu.ec>
2019-11-24 13:53:52 -0500

    implementacion mensajes

--- index.js.js
+++ index.js.js
@@ -4,10 +4,26 @@ const app = express();
 const port = process.env.PORT || 9998
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+var resp = "";
+
+app.get('/operacion/:id', function(req, res, next) {
+if (req.params.id == "editar") {
+  resp = "El Estudiante se ha editado correctamente";
+}else if (req.params.id == "eliminar") {
+  resp = "El Estudiante se ha eliminado correctamente";
+}else {
+  resp = "Se produjo un problema en la Edición/Eliminación";
+}
+  res.json({output: resp});
+});
 
 app.get('/G4T7', (req, res) => {
 res.send({ msg: 'SERVICIO GRUPO 04'}) 
 })
+
+
 app.listen(port, () => {
  console.log(`El servidor está inicializado en el puerto ${port}`);
 });
+
+module.exports = app;
