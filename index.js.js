7b34c7d0ab9690aa26fe81262bde264ac203f01e
jeffab96 <jeffab96@users.noreply.github.com>
2019-11-26 08:25:38 -0500

    Se agrega servicio de JSON a XML

--- index.js.js
+++ index.js.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 9998
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 var resp = "";
+const fs = require('fs')
+const js2xml = require("js2xmlparser")
 
 app.get('/operacion/:id', function(req, res, next) {
 if (req.params.id == "editar") {
@@ -19,6 +21,22 @@ if (req.params.id == "editar") {
 
 app.get('/G4T7', (req, res) => {
 res.send({ msg: 'SERVICIO GRUPO 04'}) 
+})
+
+app.put('/upload',function(req,res){
+    var json = req.body
+    var xml = js2xml.parse("usuario",json)
+    console.log(json)
+    console.log(xml)
+	fs.writeFile('datosEstudiantes/usuarios.xml',xml,(error)=>{	
+		if(error){
+			throw error;
+		}
+        console.log('El archivo se ha creado correctamente');
+        res.send(200)
+        res.end()
+	})
+
 })
 
 
